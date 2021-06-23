f24a63cfe16ed83c09bd514ed7275698e3151983
seanparnold <sean.p.arnold@gmail.com>
2021-06-22 23:38:04 -0700

    switched to pngs from pdfs

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,7 +4,8 @@ var currentPage = 0
 document.getElementById("nextPageButton").addEventListener("click", function(){
     if (currentPage < 47) {
         currentPage = currentPage + 1;
-        document.getElementById("PDFView").src = "./assets/pages/document-page" + currentPage + ".pdf#view=FitH&toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0";
+        console.log(currentPage)
+        document.getElementById("panelView").src = "./assets/pages/document-page" + currentPage + ".png";
     }
     else {
         window.alert('Already on the last page.')
@@ -17,7 +18,7 @@ document.getElementById("previousPageButton").addEventListener("click", function
     if (currentPage > 0) {
         currentPage = currentPage - 1;
         console.log(currentPage)
-        document.getElementById("PDFView").src = "./assets/pages/document-page" + currentPage + ".pdf#view=FitH&toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0";
+        document.getElementById("panelView").src = "./assets/pages/document-page" + currentPage + ".png";
     }
     else {
         window.alert('Already on the first page.')
